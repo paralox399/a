4d7c28a029632a615472e882a2aca534cf6b999e
paralox399 <54511577+paralox399@users.noreply.github.com>
2023-12-31 01:24:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 const express = require("express");
-const {
-  createProxyMiddleware,
-  fixRequestBody,
-} = require("http-proxy-middleware");
+const { createProxyMiddleware, fixRequestBody } = require("http-proxy-middleware");
 const bodyParser = require("body-parser");
-const expressIp = require("express-ip");
+const requestIp = require("request-ip");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(expressIp().getIpInfoMiddleware);
+app.use(requestIp.mw());
 
 const defaultTarget = "http://160.20.109.251/";
 const backupTarget = "http://185.254.238.19/";
 
-app.use("/", (req, res) => {
-  const clientIpInfo = req.ipInfo;
+app.use("/", async (req, res) => {
+  try {
+    // Get the client's IP address
+    const clientIp = req.clientIp;
 
-  // Choose the target based on the client's country
-  const target =
-    clientIpInfo && clientIpInfo.country === "TR" ? backupTarget : defaultTarget;
+    // Fetch country information using ipinfo.io API
+    const { data: ipInfo } = await axios.get(`https://ipinfo.io/${clientIp}/json`);
 
-  const proxyMiddleware = createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    onProxyReq: fixRequestBody,
-  });
+    // Choose the target based on the client's country
+    const target = ipInfo.country === "TR" ? backupTarget : defaultTarget;
 
-  proxyMiddleware(req, res, (err) => {
-    if (err) {
-      console.error("Proxy Error:", err);
-      res.status(500).send("Proxy Error");
-    }
-  });
+    const proxyMiddleware = createProxyMiddleware({
+      target,
+      changeOrigin: true,
+      onProxyReq: fixRequestBody,
+    });
+
+    proxyMiddleware(req, res, (err) => {
+      if (err) {
+        console.error("Proxy Error:", err);
+        res.status(500).send("Proxy Error");
+      }
+    });
+  } catch (error) {
+    console.error("Error fetching IP information:", error);
+    res.status(500).send("Error fetching IP information");
+  }
 });
 
 const PORT = process.env.PORT || 80;
