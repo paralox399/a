8d30347d6427fdf6d1a9915766d1a12cc808aa99
paralox399 <54511577+paralox399@users.noreply.github.com>
2024-01-03 19:54:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,19 @@
-const express = require("express");
-const { createProxyMiddleware, fixRequestBody } = require("http-proxy-middleware");
-const bodyParser = require("body-parser");
-const requestIp = require("request-ip");
-const axios = require("axios");
+const express = require('express');
+const { createProxyMiddleware, fixRequestBody } = require('http-proxy-middleware');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(requestIp.mw());
 
-const defaultTarget = "http://160.20.109.251/";
-const backupTarget = "http://185.254.238.19/";
-
-app.use("/", async (req, res) => {
-  try {
-    // Get the client's IP address
-    const clientIp = req.clientIp;
-
-    // Fetch country information using ipinfo.io API
-    const { data: ipInfo } = await axios.get(`https://ipinfo.io/${clientIp}/json`);
-
-    // Choose the target based on the client's country
-    const target = ipInfo.country === "TR" ? backupTarget : defaultTarget;
-
-    const proxyMiddleware = createProxyMiddleware({
-      target,
-      changeOrigin: true,
-      onProxyReq: fixRequestBody,
-    });
-
-    proxyMiddleware(req, res, (err) => {
-      if (err) {
-        console.error("Proxy Error:", err);
-        res.status(500).send("Proxy Error");
-      }
-    });
-  } catch (error) {
-    console.error("Error fetching IP information:", error);
-    res.status(500).send("Error fetching IP information");
-  }
+app.use('/', (req, res) => {
+    const target = "https://google.com/"; // Yönlendirilecek adres
+    if (!target) {
+        return res.sendStatus(500);
+    }
+    return createProxyMiddleware({ target: target, changeOrigin: true, onProxyReq: fixRequestBody, })(req, res);
 });
 
-const PORT = process.env.PORT || 80;
-
-app.listen(PORT, () => {
-  console.log(
-    `Reverse Proxy Başlatıldı! http://localhost:${PORT} adresinden erişebilirsiniz. (DDOS Korumalı bir sunucu üzerinde çalıştırarak kullanmanız tavsiye edilir.)`,
-  );
+app.listen(80, () => {
+    console.log('Reverse Proxy Başlatıldı! http://localhost:80 adresinden erişebilirsiniz. (DDOS Korumalı bir sunucu üzerinde çalıştırarak kullanmanız tavsiye edilir.)');
 });
